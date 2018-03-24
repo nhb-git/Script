16a4c1fc52caf7f7cb065fd4ac53c9125656aa16
niuhb2 <niuhb2@lenovo.com>
2018-03-24 13:23:32 +0800

    add functions

--- shell/Debian_System_init.sh
+++ shell/Debian_System_init.sh
@@ -4,6 +4,7 @@ source /etc/profile
 
 # install basic packages
 apt-get install ansible python3 python3-pip wget -y
+pip3 install --upgrade pip
 
 # install monaco font
 git clone https://github.com/cstrap/monaco-font
@@ -14,3 +15,6 @@ cd  monaco-font
 cd /home/nhb/×ÀÃæ/
 wget https://github.com/shadowsocks/shadowsocks-qt5/releases/download/v3.0.0/Shadowsocks-Qt5-3.0.0-x86_64.AppImage
 chmod a+x Shadowsocks-Qt5-3.0.0-x86_64.AppImage
+
+# install python3's django depend packages
+pip3 install django==1.11 pylint-django pylint autopep8

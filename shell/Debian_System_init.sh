3368cd919aaa3c0a9d4a4bc71523a7384c7b345b
niuhb2 <niuhb2@lenovo.com>
2018-03-24 14:27:20 +0800

    add functions

--- shell/Debian_System_init.sh
+++ shell/Debian_System_init.sh
@@ -2,8 +2,10 @@
 
 source /etc/profile
 
+home_dir=/home/nhb
+
 # install basic packages
-apt-get install ansible python3 python3-pip wget -y
+apt-get install ansible python3 python3-pip wget vim -y
 pip3 install --upgrade pip
 
 # install monaco font
@@ -12,9 +14,14 @@ cd  monaco-font
 ./install-font-ubuntu.sh https://github.com/todylu/monaco.ttf/blob/master/monaco.ttf?raw=true
 
 # install shadowsocks-clint
-cd /home/nhb/×ÀÃæ/
+cd $home_dir/×ÀÃæ/
 wget https://github.com/shadowsocks/shadowsocks-qt5/releases/download/v3.0.0/Shadowsocks-Qt5-3.0.0-x86_64.AppImage
 chmod a+x Shadowsocks-Qt5-3.0.0-x86_64.AppImage
 
 # install python3's django depend packages
 pip3 install django==1.11 pylint-django pylint autopep8
+pip3 install virtualenv virtualenvwrapper
+
+# add virtualenv variable to $home_dir/.bashrc
+echo "export WORKON_HOME=$home_dir/.virtualenvs" >> $home_dir/.bashrc
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> $home_dir/.bashrc

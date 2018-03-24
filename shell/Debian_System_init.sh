a4c7d5e663eee442ef2ca568da2efda3609eff25
niuhb2 <niuhb2@lenovo.com>
2018-03-24 15:18:03 +0800

    add functions

--- shell/Debian_System_init.sh
+++ shell/Debian_System_init.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 source /etc/profile
-
 home_dir=/home/nhb
+
+if [ $(whoami) != 'root' ]
+then
+	echo "please use sudo install!!!"
+	exit
+fi
 
 # install basic packages
 apt-get install ansible python3 python3-pip wget vim -y
@@ -23,5 +28,7 @@ pip3 install django==1.11 pylint-django pylint autopep8
 pip3 install virtualenv virtualenvwrapper
 
 # add virtualenv variable to $home_dir/.bashrc
+su - nhb -c "mkdir $home_dir/niuhaibao/Django_Projects"
 echo "export WORKON_HOME=$home_dir/.virtualenvs" >> $home_dir/.bashrc
+echo "export PROJECT_HOME=$home_dir/niuhaibao/Django_Projects" >> $home_dir/.bashrc
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> $home_dir/.bashrc

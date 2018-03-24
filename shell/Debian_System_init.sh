8a27a69b7f25975505e39f963b9e4d7de008d126
niuhb2 <niuhb2@lenovo.com>
2018-03-24 11:44:22 +0800

    add Debian System init script

---
+++ shell/Debian_System_init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source /etc/profile
+
+# install basic packages
+apt-get install ansible python3 python3-pip wget -y
+
+# install monaco font
+git clone https://github.com/cstrap/monaco-font
+cd  monaco-font
+./install-font-ubuntu.sh https://github.com/todylu/monaco.ttf/blob/master/monaco.ttf?raw=true
+
+# install shadowsocks-clint
+cd /home/nhb/×ÀÃæ/
+wget https://github.com/shadowsocks/shadowsocks-qt5/releases/download/v3.0.0/Shadowsocks-Qt5-3.0.0-x86_64.AppImage
+chmod a+x Shadowsocks-Qt5-3.0.0-x86_64.AppImage

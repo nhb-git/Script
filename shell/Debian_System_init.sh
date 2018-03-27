4003e4e91a08887b7eda29abb8607a3a168773e9
nhb-git <niuhaibao@gmail.com>
2018-03-27 18:34:03 +0800

    modify script

--- shell/Debian_System_init.sh
+++ shell/Debian_System_init.sh
@@ -19,7 +19,7 @@ cd  monaco-font
 ./install-font-ubuntu.sh https://github.com/todylu/monaco.ttf/blob/master/monaco.ttf?raw=true
 
 # install shadowsocks-clint
-cd $home_dir/×ÀÃæ/
+mkdir -p  $home_dir/vpn && cd $home_dir/vpn
 wget https://github.com/shadowsocks/shadowsocks-qt5/releases/download/v3.0.0/Shadowsocks-Qt5-3.0.0-x86_64.AppImage
 chmod a+x Shadowsocks-Qt5-3.0.0-x86_64.AppImage
 

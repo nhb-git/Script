4f52a51f540b7c9e041a7da95d5411a1a0dcf349
niuhb2 <niuhb2@lenovo.com>
2019-07-08 19:31:34 +0800

    add script

---
+++ python/python3/pratice/node_install.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import os
+import tarfile
+import shutil
+import subprocess
+
+
+def 
--- python/python3/pratice/test.py
+++ python/python3/pratice/test.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
-import re
-import requests
+import functools
 
 
-r = requests.get('http://louiszhai.github.io')
-re.findall('"(https?://.*?)"', r.content.decode('utf-8'))
+def log(func):
+    @functools.wraps(func)
+    def wrapper(a, b):
+        print('start execute')
+        func()
+        print('end execute')
+    return wrapper
+
+
+@log
+def my_func(a, b):
+    print('a+b=%d'.format(a+b))
+
+
+# my_func = log(my_func)
+my_func()
+print(my_func.__name__)

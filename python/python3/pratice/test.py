c14e87d2e89d601086f9849125940e035655fb07
nhb <niuhaibao@gmail.com>
2019-09-12 13:52:48 +0800

    update dir -l

--- python/python3/pratice/test.py
+++ python/python3/pratice/test.py
@@ -1,17 +1,5 @@
 # -*- coding: utf-8 -*-
-def create_counter():
-    def f():
-        i = 0
-        while True:
-            i += 1
-            yield i
-    it = f()
-
-    def counter():
-        return next(it)
-    return counter
-
-
-counter1 = create_counter()
-print(counter1())
-print(counter1())
+file_path = __file__
+with open(file_path, 'r') as f:
+    s = f.read()
+    print(s)

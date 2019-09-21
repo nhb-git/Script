04b2181843e0a92dc2f32b6320be546e36f0ba1a
nhb <niuhaibao@gmail.com>
2019-09-21 17:23:09 +0800

    update ubuntu system install docker script

---
+++ shell/ubuntu_install_docker.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+sudo apt-get remove docker docker-engine docker-ce docker.io -y
+# add image source
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+##################################################################################
+# add source's gpg secret
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# sudo apt-key fingerprint 0EBFCD88
+
+# sudo add-apt-repository \
+    # "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    # $(lsb_release -cs) \
+    # stable"
+##################################################################################
+sudo echo "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu $(lsb_release -cs) stable" \
+    >> /etc/apt/sources.list
+sudo apt-get update -y

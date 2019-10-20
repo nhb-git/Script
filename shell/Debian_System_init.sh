d4cc6cc09f8d33c90cd2a7407a3247536dd66f44
nhb-git <niuhaibao@gmail.com>
2019-10-20 10:12:58 +0800

    Update Debian_System_init.sh

--- shell/Debian_System_init.sh
+++ shell/Debian_System_init.sh
@@ -23,22 +23,27 @@ deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-security universe
 deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-security multiverse
 EOF
 
+apt-get update -y 
+sudo apt remove libappstream3
 # install basic packages
 apt-get install ansible python3 python3-pip  python3-venv python-pip git wget vim npm gcc make perl gdebi ssh-askpass bpython -y
-apt purge libreoffice-common unity-webapps-common thunderbird totem rhythmbox empathy brasero simple-scan gnome-mahjongg aisleriot gnome-mines cheese gnome-sudoku transmission-common gnome-orca webbrowser-app landscape-client-ui-install deja-dup -y
+apt purge unity-webapps-common thunderbird totem rhythmbox empathy brasero simple-scan gnome-mahjongg aisleriot gnome-mines cheese gnome-sudoku transmission-common gnome-orca webbrowser-app landscape-client-ui-install deja-dup -y
 
 # modify pip resource and update pip3
 mkdir -p ~/.pip
 echo "[global]" >> ~/.pip/pip.conf
-echo "trusted-host = pypi.douban.com" >> ~/.pip/pip.conf
-echo "index-url = https://pypi.douban.com/simple" >> ~/.pip/pip.conf
+echo "trusted-host =  mirrors.aliyun.com" >> ~/.pip/pip.conf
+echo "index-url = https://mirrors.aliyun.com/pypi/simple" >> ~/.pip/pip.conf
 
 su - nhb -c "mkdir -p ~/.pip"
 su - nhb -c 'echo "[global]" >> ~/.pip/pip.conf'
-su - nhb -c 'echo "trusted-host = pypi.douban.com" >> ~/.pip/pip.conf'
-su - nhb -c 'echo "index-url = https://pypi.douban.com/simple" >> ~/.pip/pip.conf'
+su - nhb -c 'echo "trusted-host =  mirrors.aliyun.com" >> ~/.pip/pip.conf'
+su - nhb -c 'echo "index-url = https://mirrors.aliyun.com/pypi/simple" >> ~/.pip/pip.conf'
 pip3 install --upgrade pip
-apt-get update && apt-get dist-upgrade
+# install python3's django depend packages
+pip3 install django==1.11 pylint-django pylint autopep8
+pip3 install virtualenv virtualenvwrapper
+apt-get update -y && apt-get dist-upgrade -y
 
 # install monaco font
 cd /home/nhb/Downloads
@@ -55,10 +60,6 @@ chmod a+x Shadowsocks-Qt5-3.0.0-x86_64.AppImage
 git config --global user.email "niuhaibao@gmail.com"
 git config --global user.name "nhb"
 
-# install python3's django depend packages
-pip3 install django==1.11 pylint-django pylint autopep8
-pip3 install virtualenv virtualenvwrapper
-
 # add virtualenv variable to $home_dir/.bashrc
 su - nhb -c "mkdir -p $home_dir/niuhaibao/github"
 su - nhb -c "mkdir -p $home_dir/niuhaibao/Django_Projects"
@@ -70,6 +71,5 @@ echo "alias mygit='cd $home_dir/niuhaibao/github'" >> $home_dir/.bashrc
 source $home_dir/.bashrc
 chown -R nhb.nhb $home_dir/.virtualenvs
 
-# npm install bootstrap, vue, jquery
+# npm
 su - nhb -c "npm config set registry https://registry.npm.taobao.org"
-su - nhb -c "cd $home_dir/niuhaibao/Django_Projects ; npm install bootstrap vue vue-route jquery"

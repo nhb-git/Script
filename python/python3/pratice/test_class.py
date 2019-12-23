93663a8b96b64a2ab12c4c07c25be35644ad8115
nhb <niuhaibao@gmail.com>
2019-12-23 16:11:26 +0800

    rebuild class

--- python/python3/pratice/test_class.py
+++ python/python3/pratice/test_class.py
@@ -5,21 +5,64 @@
 """
 
 
-class Athlete():
-    def __init__(self, a_name, a_dob=None, a_times=[]):
-        self.name = a_name
-        self.dob = a_dob
-        self.times = a_times
+class People():
+    name = ""
+    age = 0
+    __weight = 0
 
-    def top3(self):
-        pass
+    def __init__(self, n, a, w):
+        self.name = n
+        self.age = a
+        self.__weight = w
 
-    def add_time(self, extr_time):
-        """Add a times value to source times.
-        """
-        self.times.append(extr_time)
+    def speak(self):
+        print('%s say: he is %d' % (self.name, self.age))
 
-    def add_times(self, list_times):
-        """Add time's list to source times
-        """
-        self.times.extend(list_times)
+
+class Student(People):
+    grade = 0
+
+    def __init__(self, n, a, w, g):
+        People.__init__(self, n, a, w)
+        self.grade = g
+
+    def speak(self):
+        print(
+            '%s say: he is %d, grade is %d' % (self.name, self.age, self.grade)
+        )
+
+
+class Speaker():
+    def __init__(self, n, t):
+        self.name = n
+        self.topic = t
+
+    def speak(self):
+        print("%s is a speaker, he's topic is %s" % (self.name, self.topic))
+
+
+class Sample(Speaker, Student):
+    def __init__(self, n, a, w, g, t):
+        Student.__init__(self, n, a, w, g)
+        Speaker.__init__(self, n, t)
+        self.__private = 3
+
+    def speak(self):
+        print(self.__private)
+
+
+class Vector():
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __str__(self):
+        return 'Vector (%d, %d)' % (self.a, self.b)
+
+    def __add__(self, other):
+        return Vector(self.a + other.a, self.b + other.b)
+
+
+v1 = Vector(1, 2)
+v2 = Vector(3, 4)
+print(str(v1 + v2))

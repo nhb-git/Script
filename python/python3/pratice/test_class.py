f87475728ac98cbe5b7203f3c89adef9958a4603
nhb-git <niuhaibao@gmail.com>
2020-01-05 20:06:32 +0800

    update

---
+++ python/python3/pratice/test_class.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+class Athlete():
+    def __init__(self, a_name, a_dob=None, a_times=[]):
+        self.name = a_name
+        self.dob = a_dob
+        self.times = a_times

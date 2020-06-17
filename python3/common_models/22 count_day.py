8b5ec6a6f996c0df80f34dfa0ac294f58641b7ac
niuhb2 <niuhaibao@gmail.com>
2020-06-17 18:07:27 +0800

    统计相隔天数

---
+++ python3/common_models/22 count_day.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 开发人员   ：Davis Niu
+# 开发时间   ：6/17/2020  5:44 PM 
+# 文件名称   ：22 count_day.py
+import datetime
+
+
+birthday = datetime.date(1990, 3, 8)
+now = datetime.date.today()
+
+
+print(now-birthday)
+
